61f6c3fb96c17f8890042def3d7f07f41ff50099
harsha <manyamharsha112@gmail.com>
2021-04-06 15:30:45 +0530

    added search

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -6,7 +6,7 @@ export const SearchBox = props => (
   <input
     className='search-box'
     type='search'
-    placeholder='search monsters'
+    placeholder='search faces'
     onChange={props.onSearchChange}
   />
 );
